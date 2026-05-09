6e67d96d2154c0b2a2806141c36d4aa0ecd18dba
Sagar <sag333ar@gmail.com>
2026-05-09 08:35:16 +0530

    Deploy React app to GitHub Pages

---
+++ assets/index-Y5oeG-vL.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-sXbpec7I.js","assets/index-BwlhC7GN.js","assets/index-DxEInQ1U.css"])))=>i.map(i=>d[i]);
+import{s as e,_ as o}from"./index-BwlhC7GN.js";const t=e("Browser",{web:()=>o(()=>import("./web-sXbpec7I.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{t as Browser};
