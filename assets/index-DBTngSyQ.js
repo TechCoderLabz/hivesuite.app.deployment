ea45b7ffa01ce6833c76a218a809d0d4a4181a16
Sagar <sag333ar@gmail.com>
2026-05-11 11:54:13 +0530

    Deploy React app to GitHub Pages

---
+++ assets/index-DBTngSyQ.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BE1KlU26.js","assets/index-BDVvz2B1.js","assets/index-B0FszsgD.css"])))=>i.map(i=>d[i]);
+import{t as e,_ as o}from"./index-BDVvz2B1.js";const _=e("Browser",{web:()=>o(()=>import("./web-BE1KlU26.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
