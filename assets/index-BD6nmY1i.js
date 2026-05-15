f9de1d46080d602b639dba057f35450045aeb0a1
Sagar <sag333ar@gmail.com>
2026-05-15 16:01:02 +0530

    Deploy React app to GitHub Pages

---
+++ assets/index-BD6nmY1i.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CZkbLPu2.js","assets/index-BPV9VZi4.js","assets/index-D_feqpr2.css"])))=>i.map(i=>d[i]);
+import{_ as e,$ as o}from"./index-BPV9VZi4.js";const _=e("Browser",{web:()=>o(()=>import("./web-CZkbLPu2.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
---
+++ assets/useGameAnalytics-Dgig4X7d.js
@@ -0,0 +1,1 @@
+import{r as c,z as f,A as r}from"./index-BPV9VZi4.js";function l(u,t){c.useEffect(()=>{f(u)},[u]);const n=c.useRef(null);c.useEffect(()=>{t&&n.current!==t?(n.current=t,r(u,t)):!t&&n.current!==null&&(n.current=null)},[u,t])}export{l as u};
